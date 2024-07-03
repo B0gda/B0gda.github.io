05484f1f8b948aa9f95c6229fe1ca8d194b392a1
Bogdan Sadriev <bogda077077@gmail.com>
2024-07-03 18:18:10 +0400

    update index.html

---
+++ js/opening-CV.js
@@ -0,0 +1,37 @@
+let resumeButtons = document.querySelectorAll("#resumeButton");
+let portfolioElement = document.querySelectorAll("#portfolioElement");
+let viewprojectButton = document.querySelector(".viewProjectsButton");
+
+function isMobileDevice() {
+  const mobileRegex =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+  return mobileRegex.test(navigator.userAgent);
+}
+
+resumeButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    if (isMobileDevice()) {
+      var downloadLink = document.createElement("a");
+      downloadLink.href = "../assets/CV-S.pdf";
+      downloadLink.download = "CV-S.pdf";
+
+      if ("download" in downloadLink) {
+        downloadLink.click();
+      } else {
+        window.location.href = "../assets/CV-S.pdf";
+      }
+    } else {
+      window.open("../assets/CV-S.pdf", "_blank");
+    }
+  });
+});
+
+portfolioElement.forEach(function (button) {
+  button.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+});
+
+viewprojectButton.addEventListener("click", function () {
+  window.location.href = "https://github.com/B0gda";
+});
